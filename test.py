75e8ee23cc7019e5a8b904a53c5025fdede17d2d
Kopa Sterjik <linegod8@gmail.com>
2025-09-17 01:21:05 +0300

    C0

---
+++ test.py
@@ -0,0 +1,15 @@
+import math 
+a=int(input('Write a ='))
+b=int(input('Write b ='))
+c=int(input('Write c ='))
+
+d = b ** 2 - 4 * a * c
+
+if d > 0: 
+    x1 = (-b + math.sqrt(d)) / 2 * a
+    x2 = (-b - math.sqrt(d)) / 2 * a
+elif d==0:
+    x1 = (-b) / 2*a
+else: 
+    print('ERROR')
+    
