d1e4f9f4003e48859d964866123f69f18ca0eb62
Kopa Sterjik <linegod8@gmail.com>
2025-09-17 01:25:02 +0300

    C1

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ if d > 0:
     x1 = (-b + math.sqrt(d)) / 2 * a
     x2 = (-b - math.sqrt(d)) / 2 * a
 elif d==0:
-    x1 = (-b) / 2*a
+    x3 = (-b) / 2*a
 else: 
     print('ERROR')
-    
+print('hi')   
